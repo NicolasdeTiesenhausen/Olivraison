722cccb7d6eeec1df1ba4fcecf9a7eebf5031b8a
dkonate <dotemin@gmail.com>
2016-12-06 17:17:25 +0100

    Ajout des modifications concernant le menu
    J’ai rajouter le menu principale

--- app/src/main/java/com/example/flo/olivraison/MainActivity.java
+++ app/src/main/java/com/example/flo/olivraison/MainActivity.java
@@ -80,17 +80,21 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.Ajouter_commande) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.Liste_commande) {
 
-        } else if (id == R.id.nav_slideshow) {
+        //} else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_manage) {
+        //} else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.Ajouter_livreur) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.Liste_livreur) {
+
+        } else if (id == R.id.Set_setting){
+
+        }else if (id == R.id.Set_server){
 
         }
 
